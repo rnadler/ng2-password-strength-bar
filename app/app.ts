1dee0a83581b6a4ebd35c90ec1991283bfa9afdc
Jonas <btxtiger@outlook.com>
2017-12-16 17:41:27 +0100

    Reformat code using Webstorm code formatter

--- app/app.ts
+++ app/app.ts
@@ -1,38 +1,40 @@
-import { Component, NgModule } from '@angular/core'
-import { FormsModule } from '@angular/forms';
-import { BrowserModule } from '@angular/platform-browser';
-import { PasswordStrengthBarModule } from '../index';
+import {Component, NgModule} from '@angular/core'
+import {FormsModule} from '@angular/forms';
+import {BrowserModule} from '@angular/platform-browser';
+import {PasswordStrengthBarModule} from '../index';
 
 @Component({
-    selector: 'my-app',
-    template: `
-  <h3>Angular 2 Password Strength Bar</h3>
+  selector: 'my-app',
+  template: `
+    <h3>Angular 2 Password Strength Bar</h3>
     <div>
-       <form name="myForm" novalidate>
-            <input type="password" class="form-control" id="password" name="password" placeholder="Enter password"
-                 [(ngModel)]="account.password" #password="ngModel" 
-                 minlength="5" maxlength="50" required>
-            <ng2-password-strength-bar [passwordToCheck]="account.password" [barLabel]="barLabel"></ng2-password-strength-bar>
-        </form>        
+      <form name="myForm" novalidate>
+        <input type="password" class="form-control" id="password" name="password" placeholder="Enter password"
+               [(ngModel)]="account.password" #password="ngModel"
+               minlength="5" maxlength="50" required>
+        <ng2-password-strength-bar [passwordToCheck]="account.password"
+                                   [barLabel]="barLabel"></ng2-password-strength-bar>
+      </form>
     </div>
   `,
 })
 export class AppComponent {
-    public account = {
-        password: <string>null
-    };
-    public barLabel: string = "Password strength:";
+  public account = {
+    password: <string>null
+  };
+  public barLabel: string = "Password strength:";
 }
 
 @NgModule({
-    imports: [
-        BrowserModule,
-        FormsModule,
-        PasswordStrengthBarModule,
-    ],
-    declarations: [
-        AppComponent,
-    ],
-    bootstrap: [ AppComponent ]
+  imports: [
+    BrowserModule,
+    FormsModule,
+    PasswordStrengthBarModule,
+  ],
+  declarations: [
+    AppComponent,
+  ],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule {
+}
--- src/passwordStrengthBar.component.ts
+++ src/passwordStrengthBar.component.ts
@@ -1,115 +1,123 @@
 import {Component, OnChanges, Input, SimpleChange} from '@angular/core';
 
 @Component({
-    selector: 'ng2-password-strength-bar',
-    styles: [`
+  selector: 'ng2-password-strength-bar',
+  styles: [`
     ul#strengthBar {
-        display:inline;
-        list-style:none;
-        margin:0;
-        margin-left:15px;
-        padding:0;
-        vertical-align:2px;
+      display: inline;
+      list-style: none;
+      margin: 0;
+      margin-left: 15px;
+      padding: 0;
+      vertical-align: 2px;
     }
+
     .point:last {
-        margin:0 !important;
+      margin: 0 !important;
     }
+
     .point {
-        background:#DDD;
-        border-radius:2px;
-        display:inline-block;
-        height:5px;
-        margin-right:1px;
-        width:20px;
+      background: #DDD;
+      border-radius: 2px;
+      display: inline-block;
+      height: 5px;
+      margin-right: 1px;
+      width: 20px;
     }`],
-    template: `
+  template: `
     <div id="strength" #strength>
-        <small>{{barLabel}}</small>
-        <ul id="strengthBar">
-            <li class="point" [style.background-color]="bar0"></li><li class="point" [style.background-color]="bar1"></li><li class="point" [style.background-color]="bar2"></li><li class="point" [style.background-color]="bar3"></li><li class="point" [style.background-color]="bar4"></li>
-        </ul>
+      <small>{{barLabel}}</small>
+      <ul id="strengthBar">
+        <li class="point" [style.background-color]="bar0"></li>
+        <li class="point" [style.background-color]="bar1"></li>
+        <li class="point" [style.background-color]="bar2"></li>
+        <li class="point" [style.background-color]="bar3"></li>
+        <li class="point" [style.background-color]="bar4"></li>
+      </ul>
     </div>
-`
+  `
 })
 export class PasswordStrengthBarComponent implements OnChanges {
-    @Input() passwordToCheck: string;
-    @Input() barLabel: string;
-    bar0: string;
-    bar1: string;
-    bar2: string;
-    bar3: string;
-    bar4: string;
+  @Input() passwordToCheck: string;
+  @Input() barLabel: string;
+  bar0: string;
+  bar1: string;
+  bar2: string;
+  bar3: string;
+  bar4: string;
 
-    private colors = ['#F00', '#F90', '#FF0', '#9F0', '#0F0'];
+  private colors = ['#F00', '#F90', '#FF0', '#9F0', '#0F0'];
 
-    private static measureStrength(p: string) {
-        let _force = 0;
-        const _regex = /[$-/:-?{-~!"^_`\[\]]/g; // "
+  private static measureStrength(p: string) {
+    let _force = 0;
+    const _regex = /[$-/:-?{-~!"^_`\[\]]/g; // "
 
-        const _lowerLetters = /[a-z]+/.test(p);
-        const _upperLetters = /[A-Z]+/.test(p);
-        const _numbers = /[0-9]+/.test(p);
-        const _symbols = _regex.test(p);
+    const _lowerLetters = /[a-z]+/.test(p);
+    const _upperLetters = /[A-Z]+/.test(p);
+    const _numbers = /[0-9]+/.test(p);
+    const _symbols = _regex.test(p);
 
-        const _flags = [_lowerLetters, _upperLetters, _numbers, _symbols];
+    const _flags = [_lowerLetters, _upperLetters, _numbers, _symbols];
 
-        let _passedMatches = 0;
-        for (let _flag of _flags) {
-            _passedMatches += _flag === true ? 1 : 0;
-        }
-
-        _force += 2 * p.length + ((p.length >= 10) ? 1 : 0);
-        _force += _passedMatches * 10;
-
-        // penality (short password)
-        _force = (p.length <= 6) ? Math.min(_force, 10) : _force;
-
-        // penality (poor variety of characters)
-        _force = (_passedMatches === 1) ? Math.min(_force, 10) : _force;
-        _force = (_passedMatches === 2) ? Math.min(_force, 20) : _force;
-        _force = (_passedMatches === 3) ? Math.min(_force, 40) : _force;
-
-        return _force;
-
-    }
-    private getColor(s: number) {
-        let idx = 0;
-        if (s <= 10) {
-            idx = 0;
-        }
-        else if (s <= 20) {
-            idx = 1;
-        }
-        else if (s <= 30) {
-            idx = 2;
-        }
-        else if (s <= 40) {
-            idx = 3;
-        }
-        else {
-            idx = 4;
-        }
-        return {
-            idx: idx + 1,
-            col: this.colors[idx]
-        };
+    let _passedMatches = 0;
+    for (let _flag of _flags) {
+      _passedMatches += _flag === true ? 1 : 0;
     }
 
-    getStrengthIndexAndColor(password: string) {
-        return this.getColor(PasswordStrengthBarComponent.measureStrength(password));
-    }
+    _force += 2 * p.length + ((p.length >= 10) ? 1 : 0);
+    _force += _passedMatches * 10;
 
-    ngOnChanges(changes: {[propName: string]: SimpleChange}): void {
-        let password = changes['passwordToCheck'].currentValue;
-        this.setBarColors(5, '#DDD');
-        if (password) {
-            let c = this.getStrengthIndexAndColor(password);
-            this.setBarColors(c.idx, c.col);
-        }
+    // penality (short password)
+    _force = (p.length <= 6) ? Math.min(_force, 10) : _force;
+
+    // penality (poor variety of characters)
+    _force = (_passedMatches === 1) ? Math.min(_force, 10) : _force;
+    _force = (_passedMatches === 2) ? Math.min(_force, 20) : _force;
+    _force = (_passedMatches === 3) ? Math.min(_force, 40) : _force;
+
+    return _force;
+
+  }
+
+  private getColor(s: number) {
+    let idx = 0;
+    if (s <= 10) {
+      idx = 0;
     }
-    private setBarColors(count: number, col: string) {
-        for (let _n = 0; _n < count; _n++) {
-            this['bar' + _n] = col;
-        }
+    else if (s <= 20) {
+      idx = 1;
     }
+    else if (s <= 30) {
+      idx = 2;
+    }
+    else if (s <= 40) {
+      idx = 3;
+    }
+    else {
+      idx = 4;
+    }
+    return {
+      idx: idx + 1,
+      col: this.colors[idx]
+    };
+  }
+
+  getStrengthIndexAndColor(password: string) {
+    return this.getColor(PasswordStrengthBarComponent.measureStrength(password));
+  }
+
+  ngOnChanges(changes: { [propName: string]: SimpleChange }): void {
+    let password = changes['passwordToCheck'].currentValue;
+    this.setBarColors(5, '#DDD');
+    if (password) {
+      let c = this.getStrengthIndexAndColor(password);
+      this.setBarColors(c.idx, c.col);
+    }
+  }
+
+  private setBarColors(count: number, col: string) {
+    for (let _n = 0; _n < count; _n++) {
+      this['bar' + _n] = col;
+    }
+  }
 }
